b260b82f639a9234c5f622262753c451be11625e
collin smallegan <cmsmallegan@comsma.com>
2022-09-28 18:19:46 -0500

    Add pintrest

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,12 +2,8 @@ import { SVGProps } from "react"
 
 const navigation = {
     main: [
-        {name: 'About', href: '#'},
-        {name: 'Blog', href: '#'},
-        {name: 'Jobs', href: '#'},
-        {name: 'Press', href: '#'},
-        {name: 'Return Policy', href: '#'},
         {name: 'Privacy Policy', href: '#'},
+        {name: 'FAQ', href: '#'},
     ],
     social: [
         {
@@ -37,12 +33,24 @@ const navigation = {
             ),
         },
         {
-            name: 'Twitter',
+            name: 'Pintrest',
             href: '#',
             icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
-                <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+                <svg fill="currentColor" viewBox="0 0 511.998 511.998" {...props}>
                     <path
-                        d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"/>
+                        fillRule="evenodd"
+                        d="M405.017,52.467C369.774,18.634,321.001,0,267.684,0C186.24,0,136.148,33.385,108.468,61.39
+	c-34.114,34.513-53.675,80.34-53.675,125.732c0,56.993,23.839,100.737,63.76,117.011c2.68,1.098,5.377,1.651,8.021,1.651
+	c8.422,0,15.095-5.511,17.407-14.35c1.348-5.071,4.47-17.582,5.828-23.013c2.906-10.725,0.558-15.884-5.78-23.353
+	c-11.546-13.662-16.923-29.817-16.923-50.842c0-62.451,46.502-128.823,132.689-128.823c68.386,0,110.866,38.868,110.866,101.434
+	c0,39.482-8.504,76.046-23.951,102.961c-10.734,18.702-29.609,40.995-58.585,40.995c-12.53,0-23.786-5.147-30.888-14.121
+	c-6.709-8.483-8.921-19.441-6.222-30.862c3.048-12.904,7.205-26.364,11.228-39.376c7.337-23.766,14.273-46.213,14.273-64.122
+	c0-30.632-18.832-51.215-46.857-51.215c-35.616,0-63.519,36.174-63.519,82.354c0,22.648,6.019,39.588,8.744,46.092
+	c-4.487,19.01-31.153,132.03-36.211,153.342c-2.925,12.441-20.543,110.705,8.618,118.54c32.764,8.803,62.051-86.899,65.032-97.713
+	c2.416-8.795,10.869-42.052,16.049-62.495c15.817,15.235,41.284,25.535,66.064,25.535c46.715,0,88.727-21.022,118.298-59.189
+	c28.679-37.02,44.474-88.618,44.474-145.282C457.206,127.983,438.182,84.311,405.017,52.467z"
+                        clipRule="evenodd"
+                    />
                 </svg>
             ),
         }
@@ -53,12 +61,12 @@ export default function Footer() {
 
     return(
         <>
-            <footer className="bg-oxford-blue text-golden-rod">
+            <footer className="bg-oxford-blue text-golden-rod font-book-antiqua">
                 <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
                     <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
                         {navigation.main.map((item) => (
                             <div key={item.name} className="px-5 py-2">
-                                <a href={item.href} className="text-base text-golden hover:text-gray-300">
+                                <a href={item.href} className="text-basehover:text-gray-300">
                                     {item.name}
                                 </a>
                             </div>
@@ -66,13 +74,13 @@ export default function Footer() {
                     </nav>
                     <div className="mt-8 flex justify-center space-x-6">
                         {navigation.social.map((item) => (
-                            <a key={item.name} href={item.href} className="text-golden hover:text-gray-300">
+                            <a key={item.name} href={item.href} className="hover:text-gray-300">
                                 <span className="sr-only">{item.name}</span>
                                 <item.icon className="h-6 w-6" aria-hidden="true" />
                             </a>
                         ))}
                     </div>
-                    <p className="mt-8 text-center text-base text-golden">&copy; 2022 Yahtzmen Furnishings All rights reserved.</p>
+                    <p className="mt-8 text-center text-base">&copy; 2022 Yahtzmen Furnishings All rights reserved.</p>
                 </div>
             </footer>
         </>
