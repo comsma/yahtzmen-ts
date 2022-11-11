5f7d69071693fcb6917825254638d3efd1f23e28
collin smallegan <cmsmallegan@comsma.com>
2022-11-10 23:28:46 -0600

    Experimental runtime

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['yahtzmen-storage.nyc3.cdn.digitaloceanspaces.com', 'yahtzmen-storage.nyc3.digitaloceanspaces.com']
     },
+    experimental: {
+        runtime: 'experimental-edge',
+    },
     reactStrictMode: true,
     swcMinify: true
 }
