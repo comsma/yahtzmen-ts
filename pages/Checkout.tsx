95c4c7e5a1ac8ba5ef035c700758ec2b0937d3ae
collin smallegan <cmsmallegan@comsma.com>
2022-11-11 16:44:00 -0600

    Cart fix

--- pages/Checkout.tsx
+++ pages/Checkout.tsx
@@ -4,7 +4,7 @@ import {Fragment} from "react";
 import {TrashIcon} from "@heroicons/react/24/solid";
 import {removeItem} from "../features/cartSlice";
 import {MinusCircleIcon, PlusCircleIcon} from "@heroicons/react/20/solid";
-import Image from "next/future/image";
+import Image from "next/image";
 import {ShoppingBagIcon} from "@heroicons/react/24/outline";
 
 const Checkout: NextPage =() => {
@@ -15,8 +15,8 @@ const Checkout: NextPage =() => {
         <div className={'min-h-screen'}>
             <h1 className={'py-5 font-bold text-3xl text-center '}> Checkout </h1>
             {cart?.map((item) => (
-                <div className={'mx-auto my-5 p-5 max-w-3xl flex flex-row bg-gray-200 rounded-xl'} key={item.itemId}>
-                    <div className={'grid grid-cols-3 place-items-center'}>
+                <div className={'mx-auto my-5 p-5 max-w-3xl grid grid-cols-5 grid-rows-1 bg-gray-200 rounded-xl'} key={item.itemId}>
+                    <div className={'col-span-3 grid grid-cols-3 place-items-center'}>
                         <div className={'col-span-1'}>
                             <p>{item.name}</p>
                         </div>
@@ -30,9 +30,13 @@ const Checkout: NextPage =() => {
                         </div>
                     </div>
 
-
-                    <div className={'ml-auto relative rounded-md overflow-hidden'}>
-                        <Image width={100} height={100} src={item.itemImg} />
+                    <div className={'col-start-4 col-span-1 ml-auto relative min-h-80 w-full aspect-square rounded-md overflow-clip'}>
+                        <Image
+                            layout={'fill'}
+                            objectFit={'cover'}
+                            src={item.itemImg}
+                            alt={item.name}
+                        />
                     </div>
 
 
